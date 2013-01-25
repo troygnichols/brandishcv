5513a1e1fccb29229f92f02ee1dd5b000a6e125e
Troy Nichols <tnichols@onsitehd.com>
2013-01-24 18:12:38 -0600

    fixed CSS and other things

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20130121175439) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
