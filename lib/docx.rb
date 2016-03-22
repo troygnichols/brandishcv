8fedbd48006d2863a2fd1a7a1004720f5d3a1899
Troy Nichols <troygnichols@gmail.com>
2016-03-22 01:57:56 -0500

    Fix docx generator

--- lib/docx.rb
+++ lib/docx.rb
@@ -5,7 +5,7 @@ class Docx
     File.open tmp_markdown_file, 'w' do |md_file|
       md_file.puts cv.markdown
     end
-    `pandoc "#{tmp_markdown_file} -o "#{output_docx_file}"`
+    `pandoc "#{tmp_markdown_file}" -o "#{output_docx_file}"`
     output_docx_file
   end
 end
